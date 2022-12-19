d001a7398b62fd17218c5d46f739f47d38a1f9bd
Moussa Diop <moussadiop2005@hotmail.com>
2022-12-19 16:25:11 +0000

    deployed website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { marked } from "marked";
+
+// a heading element (H1 size), a sub heading element (H2 size),
+// a link, inline code, a code block, a list item,
+// a blockquote, an image, and bolded text.
 
 function App() {
+  const [code, setCode] = useState(`
+  # Welcome to my React Markdown Previewer!
+
+  ## This is a sub-heading
+
+  [title](https://www.freecodecamp.org)
+
+  \`<div></div>\`
+  \`\`\`
+  {
+    "firstName": "John",
+    "lastName": "Smith",
+    "age": 25
+  }
+  \`\`\`
+
+  - First item
+  - Second item
+  - Third item
+
+  > blockquote
+
+  **bold text**
+
+  ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+
+  `);
+
+  marked.setOptions({
+    breaks: true,
+  });
+
+  const handleChange = (e) => {
+    setCode(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        id="editor"
+        cols="30"
+        rows="10"
+        onChange={handleChange}
+        value={code}
+      ></textarea>
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{
+          __html: marked(code),
+        }}
+      ></div>
     </div>
   );
 }
